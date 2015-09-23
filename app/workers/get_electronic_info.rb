4f05f43643afbfabc5a99d5e505656e567909263
Hiroki Ariji <ariji.hiroki@O-05166-MAC.local>
2015-09-24 08:48:24 +0900

    add new function : open API

---
+++ app/workers/get_electronic_info.rb
@@ -0,0 +1,14 @@
+
+
+# 東京電力管轄内の最新の電力使用状況を取得する場合
+http://setsuden.yahooapis.jp/v1/Setsuden/latestPowerUsage?appid=dj0zaiZpPVMzb2F4eERaZG9tYSZzPWNvbnN1bWVyc2VjcmV0Jng9MzI-&area=tokyo
+
+# 東京電力管轄内の2011年6月1日14時台を平均した電力使用状況を取得する場合
+http://setsuden.yahooapis.jp/v1/Setsuden/latestPowerUsage?appid=dj0zaiZpPVMzb2F4eERaZG9tYSZzPWNvbnN1bWVyc2VjcmV0Jng9MzI-&area=tokyo&datetime=2011060114
+
+# 東北電力管轄内の5分間隔で更新される最新の電力使用状況を取得する場合
+http://setsuden.yahooapis.jp/v1/Setsuden/latestPowerUsage?appid=dj0zaiZpPVMzb2F4eERaZG9tYSZzPWNvbnN1bWVyc2VjcmV0Jng9MzI-&area=tohoku&latest=1
+
+
+
+
