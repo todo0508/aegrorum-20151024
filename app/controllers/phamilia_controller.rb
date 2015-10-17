e0c438f07a6bf414e68cf45970993a36ae5f4645
Hiroki Ariji <ariji.hiroki@O-05166-MAC.local>
2015-10-17 23:16:18 +0900

    cooking scroll

--- app/controllers/phamilia_controller.rb
+++ app/controllers/phamilia_controller.rb
@@ -280,7 +280,13 @@ class PhamiliaController < ApplicationController
           :text5 => "別府の温泉宿の料理です", :text6 => "別府の温泉宿の料理です", :text7 => "別府の温泉宿の料理です", :text8 => "別府の温泉宿の料理です", :text9 => "別府の温泉宿の料理です"}
 
       when "5"  # グルメ情報を取得
-        json_data = {:text => "別府の温泉宿の料理です", :apn => "http://yahoo.co.jp", :interest => 0}
+        text,apn,interest = CookingCollect::GetMenu.getFoodStore
+        if text.length > 0
+          json_d = {:text => text, :apn => apn, :interest => interest}
+        else
+          json_d = {:text => "松阪肉牛・近江肉牛販売指定店\n焼肉 京城 水道橋店\n\n★水道橋駅徒歩2分★焼肉店で初めて！松阪牛・近江牛販売指定店の看板を許された老舗です。■宴会コースは4000円～ご用意！", :apn => "http://s.tabelog.com/tokyo/A1310/A131003/13000222/", :interest => 0}
+        end
+        json_data = json_d
 
       when "6"  # 書籍情報を取得
         json_data = {:text => "坊ちゃん", :apn => "http://yahoo.co.jp", :interest => 0}
--- app/workers/cooking_collect.rb
+++ app/workers/cooking_collect.rb
@@ -1,3 +1,5 @@
+require 'rexml/document'
+
 
 module CookingCollect
 
@@ -6,18 +8,53 @@ module CookingCollect
 
 
   	def self.getFoodStore
+        response = PhamiliaBackend::CollectHouse.home_user
+        doc = REXML::Document.new(response)
+        latitude = doc.elements['HemsUser/Latitude'].text
+        longitude = doc.elements['HemsUser/Longitude'].text
+
+        web_command = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=76bf55ead0c8e21a&lat=" << latitude << "&lng=" << longitude << "&range=5&order=4"
+        result = `curl $'#{web_command}'`
+        doc = REXML::Document.new(result)
+        results_available = doc.elements['results/results_returned'].text.to_i
+
+        random = Random.new
+		index = random.rand(0..(results_available-1))
+        text = ""
+        apn = ""
+        interest = 0
+        i = 0
+		doc.elements.each('results/shop') do |sle|
+			if i == index
+	        	text = "店名：" << sle.elements['name'].text << "\n住所：" << \
+	        		sle.elements['address'].text << "\n" << sle.elements['genre/name'].text << "\n" << \
+	           		sle.elements['genre/catch'].text << "\n" << sle.elements['catch'].text << "\n" << sle.elements['child'].text
+	        	apn = sle.elements['coupon_urls'].text
+	        	if apn == nil
+		        	apn = sle.elements['urls/mobile'].text
+	        	end
+	        	interest = 1
+	        end
+        	i += 1
+        end
+
+        return text,apn,interest
+	end
+
+  	def self.getFoodStore_ori
       web_command = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=76bf55ead0c8e21a&lat=" << 34.67 << "&lng=" << 135.52 << "&range=5&order=4"
       `curl $'#{web_command}'`
 	end
 
+
   	def self.getFood
-      web_command = "http://webservice.recruit.co.jp/hotpepper/food/v1/"
+      web_command = "http://webservice.recruit.co.jp/hotpepper/food/v1/?key=76bf55ead0c8e21a"
       `curl $'#{web_command}'`
 	end
 
 
   	def self.getFoodCategory
-      web_command = "http://webservice.recruit.co.jp/hotpepper/food_category/v1/"
+      web_command = "http://webservice.recruit.co.jp/hotpepper/food_category/v1/?key=76bf55ead0c8e21a"
       `curl $'#{web_command}'`
 	end
 
